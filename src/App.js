990f39df3060aef9e3a2af697cf34abd97a1ee9c
chisnallc <chisnallc@hotmail.com>
2019-01-03 21:19:28 +0000

    changed background

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Header from'./components/Header';
+import Header from './Components/Header';
+import TaskList from './Components/TaskList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div style={ sectionStyle}>
+      <Header />
+             </div>
     );
   }
 }
 
+var sectionStyle = {
+  width: "100%",
+  height: "800px",
+  backgroundImage: 'url("https://ccbutcher.com/wp-content/uploads/2016/04/HN_BB_05_EasyCooking_ProRes_739-img_1280x720.png")'
+};
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,18 @@ import React from 'react';
 class Header extends React.Component {
 
     render() {
-              return(<h1>Beautiful Recipes To Do in 10 Days</h1>);
+              return (<h1 style={styles.header}>Beautiful Recipes To Do in 10 Days</h1>);
     }
 }
+
+
+const styles = {
+    header : {
+        color: "#EA172A",
+        textAlign: 'center',
+        fontSize: '60px'
+        
+    }
+};
 
 export default Header;
