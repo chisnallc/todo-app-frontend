6d578018ba7002f32851d900ce769f3e2bf75040
chisnallc <chisnallc@hotmail.com>
2019-01-03 17:17:09 +0000

    error message

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Header extends React.Component {
+
+    render() {
+              return(<h1>Beautiful Recipes To Do in 10 Days</h1>);
+    }
+}
+
+export default Header;
