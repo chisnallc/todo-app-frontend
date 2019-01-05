9b0cbe834e8ea7fa8e8a3ac63df7b77ef4cbd85a
chisnallc <chisnallc@hotmail.com>
2019-01-05 14:22:57 +0000

    format

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div style={ sectionStyle}>
+      <div>
       <Header />
+      <TaskList />
              </div>
     );
   }
 }
 
-var sectionStyle = {
-  width: "100%",
-  height: "800px",
-  backgroundImage: 'url("https://ccbutcher.com/wp-content/uploads/2016/04/HN_BB_05_EasyCooking_ProRes_739-img_1280x720.png")'
-};
+
 export default App;
