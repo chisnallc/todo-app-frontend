2765993a5424b437cea4f3ccfbbbd7f55194cb4d
chisnallc <chisnallc@hotmail.com>
2019-01-07 12:21:57 +0000

    using james code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import Header from './Components/Header';
-import TaskList from './Components/TaskList';
-import './App.css';
+import Header from './components/Header';
+import TaskList from './components/TaskList';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <Header />
-      <TaskList />
-             </div>
+      <div className="container">
+        <Header />
+        <TaskList />
+      </div>
     );
   }
 }
-
 
 export default App;
