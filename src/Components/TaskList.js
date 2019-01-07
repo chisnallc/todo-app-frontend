9f5998237dd234d8bfdc2f6502f0783928ff391c
chisnallc <chisnallc@hotmail.com>
2019-01-07 12:23:48 +0000

    using james Code

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,35 +1,40 @@
 import React from 'react';
-import Task from './Task';
 
-class TaskList extends React.Component {
+ import Task from './Task';
 
-    render() {
+ class TaskList extends React.Component {
 
-        let listOfTasks = [
-            "Duck Ragu",
-            "Cowboy Chilli",
-            "Sausage Casserole"
+     render() {
 
+         let listOfTasks = [ 
+            {
+                id: 1,
+                description: "Take dog for a walk",
+                completed: false
+            },
+            {
+                id: 2,
+                description: "By a bottle of Milk",
+                completed: false
+            },
+            {
+                id: 3,
+                description: "Go to bed",
+                completed: false
+            },
         ];
 
-        return (
-
+         return (
             <div>
                 {
-                    listOfTasks.map((task, i) =>
-                        <Task taskDescription={task} key={i} />
-
+                    listOfTasks.map((task, i) => 
+                        <Task taskDescription={task.description} key={i} />
                     )
-
-
                 }
             </div>
         );
     }
 
+ }
 
-}
-
-
-
-export default TaskList;
+ export default TaskList;
