da69007ad0c235f2715368b11a8aa43d54184375
chisnallc <chisnallc@hotmail.com>
2019-01-08 16:10:41 +0000

    addition of tasks

---
+++ src/Components/TaskEntry.js
@@ -0,0 +1,66 @@
+import React from 'react';
+class TaskEntry extends React.Component {
+
+    constructor(props) {
+        super('props');
+
+        this.state = {
+            taskDescription: ""
+        };
+
+        this.onSavedClicked = this.onSavedClicked.bind(this);
+        this.onTaskTextFieldUpdated = this.onTaskTextFieldUpdated.bind(this);
+    }
+
+
+    onSavedClicked() {
+
+        const taskToBeAdded = {
+            id: (Math.random() * 100),
+            description: this.state.taskDescription,
+            completed: false
+        };
+        this.props.onSaveTaskHandler(taskToBeAdded);
+
+
+        this.setState({
+            taskDescription: ""
+        })
+    }
+
+
+    onTaskTextFieldUpdated(event) {
+
+        const description = event.target.value;
+        this.setState({
+            taskDescription: description
+        });
+    }
+    render() {
+        return (
+            <div className="row">
+                <div classname="col">
+                    <input type="text" value={this.state.taskDescription} onChange={this.onTaskTextFieldUpdated} />
+                </div>
+                <div className="col">
+                    <input type="button" value="save" onClick={this.onSavedClicked} />
+                </div>
+            </div>
+
+
+
+
+
+
+        );
+
+
+
+
+
+    }
+}
+
+
+
+export default TaskEntry;
