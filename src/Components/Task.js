b5ecd9aa264bbeadaa71547a5d37e2077434f306
chisnallc <chisnallc@hotmail.com>
2019-01-10 13:29:05 +0000

    style and button amends

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,30 +1,45 @@
 import React from 'react';
 
- class Task extends React.Component {
 
-     render() {
+class Task extends React.Component {
+
+    render() {
         return (
             <div className="row" style={styles.taskRow}>
-                <div className="col">
+                <div className="col" style={styles.taskDescription}>
                     {this.props.taskDescription}
                 </div>
-                <div className="col">
-                    <input type="button" value="Delete" />
+                <div className="col" >
+                    <input type="button" class= "btn btn-outline-success" value="Delete"style={styles.buttonStyle}  />
+                    {this.props.buttonStyle}
                 </div>
                 <div className="col">
-                    <input type="button" value="Done" />
+                    <input type="button" class= "btn btn-outline-success" value="Done" style={styles.buttonStyle} />
+                    {this.props.buttonStyle}  
                 </div>
             </div>
         );
     }
+    
+}
 
- }
-
- const styles = {
+const styles = {
     taskRow: {
         paddingTop: "10px",
         paddingBottom: "10px"
+    },
+    taskDescription: {
+        fontFamily: "Monospace",
+        fontWeight: "bold",
+        color: "brown",
+        fontSize: "20px"
+
+    },
+    buttonStyle: {
+        color: "brown"
     }
 }
 
- export default Task;
+
+
+export default Task;
