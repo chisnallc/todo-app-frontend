102ea37528e5f04b0b0aa8b209a3ae68e0d73e81
chisnallc <chisnallc@hotmail.com>
2019-01-23 11:30:55 +0000

    fix to counter

--- src/Components/taskCount.js
+++ src/Components/taskCount.js
@@ -6,15 +6,24 @@ class taskCount extends React.Component {
 
         return (
 
-            <div >
-            
-            {this.props.tasks.length}
-            
+            <div style={styles.taskCount}>
+                <h2>Task Counter</h2>
+                            
+                    { this.props.tasks.length }
+
+
+                
             </div>
         );
     }
 }
 
+const styles = {
 
+    taskCount: {
+
+        font: "40px",
+    },
+}
 
 export default taskCount;
